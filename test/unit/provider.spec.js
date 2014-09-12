4d545937b304ca44a0efbf0caac487db76d7b4f6
Mark Haussmann <mark.haussmann@gmail.com>
2014-09-12 15:08:31 +1200

    Initial commit

---
+++ test/unit/provider.spec.js
@@ -0,0 +1,56 @@
+describe('blink provider', function() {
+  beforeEach(module('ajoslin.promise-tracker'));
+  beforeEach(module('mhBlink'));
+
+  var timeout, q, scope, compile;
+  beforeEach(inject(function($timeout, $q, $rootScope, $compile) {
+    timeout = $timeout;
+    q = $q;
+    scope = $rootScope;
+    compile = $compile;
+  }));
+
+  function digest() {
+    inject(function($rootScope) { $rootScope.$digest(); });
+  }
+
+  it('should retain original functionality', function() {
+    var link;
+
+    scope.msg = 'Run for 3s';
+    var cb = function(){ scope.msg = 'Just ran!'; };
+    scope.runFor = function(secs){
+      scope.msg = 'Running...';
+      var timer = timeout(cb, parseInt(secs, 10) * 1000);
+
+      // timeout(function(){
+      //   timeout.cancel(timer);
+      // }, 1000);
+
+      return timer;
+    };
+
+    link = compile('<a ng-click="runFor(3)" blink-on="ngClick">{{msg}}</a>')(scope);
+    digest();
+    link.triggerHandler('click');
+    digest();
+    expect(link.html()).toContain('Running...');
+    timeout.flush();
+    expect(link.html()).toContain('Just ran!');
+  });
+
+  it('should add element when loading', function() {
+    var link;
+    scope.runFor = function(secs){
+      return timeout(angular.noop, parseInt(secs, 10) * 1000);
+    };
+
+    link = compile('<a ng-click="runFor(3)" blink-on="ngClick">{{msg}}</a>')(scope);
+    link.triggerHandler('click');
+    digest();
+    expect(link.find('b').length).toEqual(4);
+    timeout.flush();
+    expect(link.find('b').length).toEqual(0);
+  });
+
+});
